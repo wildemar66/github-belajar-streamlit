4af9e04e734c857ed13347543f9d37f2874088a5
wildemar <wildemarminggu66@gmail.com>
2025-04-24 17:55:20 +0700

    membuat file kontak.py

---
+++ kontak.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+def display_contact():
+    st.header("Hubungi Saya")
+    
+    with st.form(key='contact_form'):
+        name = st.text_input("Nama Lengkap")
+        email = st.text_input("Email")
+        message = st.text_area("Pesan")
+        submit_button = st.form_submit_button("Kirim Pesan")
+        
+        if submit_button:
+            if name and email and message:
+                st.success("Pesan Anda telah terkirim! Saya akan segera menghubungi Anda.")
+                # Di sini Anda bisa menambahkan kode untuk mengirim email atau menyimpan pesan
+            else:
+                st.warning("Harap isi semua field!")
+                
+    st.markdown("""
+    **Media Sosial:**  
+    - [LinkedIn](www.linkedin.com/in/wildemarminggu.com)  
+    - [GitHub](https://github.com/wildemar66/github-belajar-streamlit)  
+    """)
